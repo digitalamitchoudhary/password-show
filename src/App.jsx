189be8526fcef04a2761f8554d879607d05ea207
digitalamitchoudhary <digitalamitchoudhary@gmail.com>
2024-11-20 23:11:21 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+ 
+import './App.css'
+import ShowPassword from './components/ShowPassword'
+
+function App() {
+   
+
+  return (
+    <>
+   <div className='heading'> <h1 >Show Password</h1></div>
+     <ShowPassword></ShowPassword>
+    </>
+  )
+}
+
+export default App
